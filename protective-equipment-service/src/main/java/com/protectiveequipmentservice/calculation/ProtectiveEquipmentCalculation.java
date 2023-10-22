ab7945873a8f6561f7b41859abac75bc884d9775
EllsaG <seninlsa@gmail.com>
2023-10-23 00:18:57 +0400

    STEP-17: Design of calculation methods (Protective equipment)

---
+++ protective-equipment-service/src/main/java/com/protectiveequipmentservice/calculation/ProtectiveEquipmentCalculation.java
@@ -0,0 +1,47 @@
+package com.protectiveequipmentservice.calculation;
+
+
+import com.protectiveequipmentservice.entity.LowVoltCables;
+import com.protectiveequipmentservice.entity.ProtectiveEquipment;
+import com.protectiveequipmentservice.entity.ProtectiveEquipmentSelection;
+import com.protectiveequipmentservice.exeptions.InformationNotFoundException;
+import com.protectiveequipmentservice.repository.ProtectiveEquipmentSelectionRepository;
+import com.protectiveequipmentservice.rest.StartInformationClient;
+
+public class ProtectiveEquipmentCalculation {
+
+
+    public ProtectiveEquipmentSelection selectionProtectiveEquipment(short startInformationId,
+                                                                     float activePower, float cosf) {
+        final float efficiencyFactor = 0.87F; // depends on type of equipment
+        final float voltage = 0.38F;
+        final float coefIncreasingInrushCurrent = 5; // depends on type of equipment
+
+        float nominalCurrentOfEquipment = (float) (Math.round(activePower /
+                        (Math.sqrt(3) * voltage * cosf * efficiencyFactor) * 100) / 100.0);
+        float startingCurrentOfEquipment = Math.round((nominalCurrentOfEquipment * coefIncreasingInrushCurrent * 100)/100.0);
+        float calculatedCurrentOfThermalRelease = Math.round((1.15 * nominalCurrentOfEquipment * 100)/100.0);
+        float calculatedCurrentOfElectromagneticRelease = Math.round((1.25 * startingCurrentOfEquipment * 100)/100.0);
+
+        return new ProtectiveEquipmentSelection(startInformationId,nominalCurrentOfEquipment, startingCurrentOfEquipment,
+                calculatedCurrentOfThermalRelease, calculatedCurrentOfElectromagneticRelease);
+    }
+
+
+    public ProtectiveEquipment createNew(short id, float nominalCurrentOfThermalRelease, float nominalCurrentOfElectromagneticRelease,
+                                         float nominalCurrentOfCircuitBreaker, String typeOfCircuitBreaker, String cableType, StartInformationClient startInformationClient,
+                                         ProtectiveEquipmentSelectionRepository protectiveEquipmentSelectionRepository) {
+
+        if (startInformationClient.checkAvailability(id) == 0){
+            throw new InformationNotFoundException("Unable to find information about the protected equipment. Check the availability of this equipment.");
+        }
+
+        protectiveEquipmentSelectionRepository.findById(id)
+                .orElseThrow(() -> new InformationNotFoundException("Unable to find information about the protected equipment. Check the availability of this equipment."));
+
+        return new ProtectiveEquipment(id, typeOfCircuitBreaker, nominalCurrentOfThermalRelease, nominalCurrentOfElectromagneticRelease,
+                nominalCurrentOfCircuitBreaker, new LowVoltCables(cableType));
+    }
+
+
+}
---
+++ start-information-service/src/main/java/com/startinformationservice/rest/ProtectiveEquipmentRequestDTO.java
@@ -0,0 +1,14 @@
+package com.startinformationservice.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProtectiveEquipmentRequestDTO {
+
+    private short startInformationId;
+    private float activePower;
+    private float cosf;
+
+}
