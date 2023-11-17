1393ea6c1cba35606891742699c08c11ca6e89f1
EllsaG <114601257+EllsaG@users.noreply.github.com>
2023-11-18 03:32:35 +0400

    STEP-27: Create unit and integration tests for "Compensation device" (#69)
    Co-authored-by: EllsaG <seninlsa@gmail.com>

--- compensation-device-service/src/main/java/com/project/compensationdevice/calculation/CompensationDeviceCalculation.java
+++ compensation-device-service/src/main/java/com/project/compensationdevice/calculation/CompensationDeviceCalculation.java
@@ -10,15 +10,15 @@ import com.project.compensationdevice.rest.FullInformationResponseDTO;
 import java.util.NoSuchElementException;
 
 public class CompensationDeviceCalculation {
-    private static final float coefTakingIncreasingCosf = 0.9F;
+    private static final float coefTakingIncreasingCosF = 0.9F;
 
     public CompensationDeviceSelection createCompensationDeviceSelectionInformation(short compensationDeviceId, float avgDailyActivePower, float tgfBeforeCompensation) {
         float minTgfRecommendedAfterCompensation = 0.33F;
         float maxTgfRecommendedAfterCompensation = 0.4F;
 
-        float maxPowerOfCompensatingDevice =  (Math.round(avgDailyActivePower * coefTakingIncreasingCosf
+        float maxPowerOfCompensatingDevice =  (Math.round(avgDailyActivePower * coefTakingIncreasingCosF
                         * (tgfBeforeCompensation - minTgfRecommendedAfterCompensation) * 100.0) / 100.0F);
-        float  minPowerOfCompensatingDevice= (Math.round(avgDailyActivePower * coefTakingIncreasingCosf
+        float  minPowerOfCompensatingDevice= (Math.round(avgDailyActivePower * coefTakingIncreasingCosF
                         * (tgfBeforeCompensation - maxTgfRecommendedAfterCompensation) * 100.0) / 100.0F);
 
         return new CompensationDeviceSelection(compensationDeviceId, minPowerOfCompensatingDevice, maxPowerOfCompensatingDevice);
@@ -33,12 +33,13 @@ public class CompensationDeviceCalculation {
         float tgfBeforeCompensation = fullInformation.getFullInformation().getTgF();
 
         float tgfActualValueCheck = (tgfBeforeCompensation - powerOfCompensatingDevice
-                        / (coefTakingIncreasingCosf * avgDailyActivePower));
+                        / (coefTakingIncreasingCosF * avgDailyActivePower));
 
         if (tgfActualValueCheck >=0.33 && tgfActualValueCheck <= 0.4){
             return new CompensationDevice(compensationDeviceId, nameOfCompensationDevice, powerOfCompensatingDevice);
         }else {
-            CompensationDeviceSelection compensationDeviceSelection = compensationDeviceSelectionRepository.findById(compensationDeviceId).orElseThrow(() -> new NoSuchElementException("No value present"));
+            CompensationDeviceSelection compensationDeviceSelection = compensationDeviceSelectionRepository.findById(compensationDeviceId)
+                    .orElseThrow(() -> new NoSuchElementException("No value present"));
             float minPowerOfCompensatingDevice = compensationDeviceSelection.getMinPowerOfCompensatingDevice();
             float maxPowerOfCompensatingDevice = compensationDeviceSelection.getMaxPowerOfCompensatingDevice();
             throw new IncorrectNumberValueException("Incorrect power value of the compensating device, as it should be between "
--- compensation-device-service/src/main/java/com/project/compensationdevice/controller/dto/CompensationDeviceResponseDTO.java
+++ compensation-device-service/src/main/java/com/project/compensationdevice/controller/dto/CompensationDeviceResponseDTO.java
@@ -5,12 +5,14 @@ import com.project.compensationdevice.entity.CompensationDevice;
 import com.project.compensationdevice.entity.CompensationDeviceSelection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CompensationDeviceResponseDTO {
     List<CompensationDevice> compensationDeviceList;
     List<CompensationDeviceSelection> compensationDeviceSelectionList;
