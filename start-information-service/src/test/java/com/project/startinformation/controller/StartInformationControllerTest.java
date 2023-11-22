db17e65ca6eed8a6bf844d2f6ef19dc08e8bef47
EllsaG <seninlsa@gmail.com>
2023-11-23 03:46:45 +0400

    STEP-23: Create unit and integration tests for "Start information"

--- start-information-service/src/test/java/com/project/startinformation/controller/StartInformationControllerTest.java
+++ start-information-service/src/test/java/com/project/startinformation/controller/StartInformationControllerTest.java
@@ -13,12 +13,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -26,10 +25,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -46,6 +45,7 @@ public class StartInformationControllerTest {
 
     @MockBean
     private ProtectiveEquipmentServiceClient protectiveEquipmentServiceClient;
+
     @Before
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -53,7 +53,84 @@ public class StartInformationControllerTest {
 
 
     @Test
-    public void saveStartInformation() throws Exception {
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql", "/sql/addStartInformation.sql"})
+    public void getStartInformationByIdList() throws Exception {
+
+
+        MvcResult mvcResult = mockMvc.perform(get("/getInformationByIdList/?eq=2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        StartInformationResponseDTO startInformationResponseList = objectMapper.readValue(body, StartInformationResponseDTO.class);
+        StartInformation startInformationResponse = startInformationResponseList.getStartInformationList().get(0);
+
+
+        StartInformation startInformationRepositoryById = startInformationRepository.findById((short) 2)
+                .orElseThrow(() -> new NoSuchElementException("No value present"));
+
+        Assertions.assertEquals(startInformationRepositoryById.getStartInformationId(), startInformationResponse.getStartInformationId());
+        Assertions.assertEquals(startInformationRepositoryById.getName(), startInformationResponse.getName());
+        Assertions.assertEquals(startInformationRepositoryById.getActivePower(), startInformationResponse.getActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAmount(), startInformationResponse.getAmount());
+        Assertions.assertEquals(startInformationRepositoryById.getKi(), startInformationResponse.getKi());
+        Assertions.assertEquals(startInformationRepositoryById.getCosf(), startInformationResponse.getCosf());
+        Assertions.assertEquals(startInformationRepositoryById.getTgf(), startInformationResponse.getTgf());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyActivePower(), startInformationResponse.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyReactivePower(), startInformationResponse.getAvgDailyReactivePower());
+
+    }
+
+
+    @Test
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql", "/sql/addStartInformation.sql"})
+    public void getAllStartInformation() throws Exception {
+
+
+        MvcResult mvcResult = mockMvc.perform(get("/getAllInformation")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        StartInformationResponseDTO startInformationResponseList = objectMapper.readValue(body, StartInformationResponseDTO.class);
+        StartInformation startInformationResponse1 = startInformationResponseList.getStartInformationList().get(0);
+        StartInformation startInformationResponse2 = startInformationResponseList.getStartInformationList().get(1);
+
+        List<StartInformation> startInformationRepositoryAll = startInformationRepository.findAll();
+        StartInformation startInformation1 = startInformationRepositoryAll.get(0);
+        StartInformation startInformation2 = startInformationRepositoryAll.get(1);
+
+        Assertions.assertEquals(startInformation1.getName(), startInformationResponse1.getName());
+        Assertions.assertEquals(startInformation1.getActivePower(), startInformationResponse1.getActivePower());
+        Assertions.assertEquals(startInformation1.getAmount(), startInformationResponse1.getAmount());
+        Assertions.assertEquals(startInformation1.getKi(), startInformationResponse1.getKi());
+        Assertions.assertEquals(startInformation1.getCosf(), startInformationResponse1.getCosf());
+        Assertions.assertEquals(startInformation1.getTgf(), startInformationResponse1.getTgf());
+        Assertions.assertEquals(startInformation1.getAvgDailyActivePower(), startInformationResponse1.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformation1.getAvgDailyReactivePower(), startInformationResponse1.getAvgDailyReactivePower());
+
+        Assertions.assertEquals(startInformation2.getName(), startInformationResponse2.getName());
+        Assertions.assertEquals(startInformation2.getActivePower(), startInformationResponse2.getActivePower());
+        Assertions.assertEquals(startInformation2.getAmount(), startInformationResponse2.getAmount());
+        Assertions.assertEquals(startInformation2.getKi(), startInformationResponse2.getKi());
+        Assertions.assertEquals(startInformation2.getCosf(), startInformationResponse2.getCosf());
+        Assertions.assertEquals(startInformation2.getTgf(), startInformationResponse2.getTgf());
+        Assertions.assertEquals(startInformation2.getAvgDailyActivePower(), startInformationResponse2.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformation2.getAvgDailyReactivePower(), startInformationResponse2.getAvgDailyReactivePower());
+    }
+
+
+    @Test
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql",})
+    public void createStartInformation() throws Exception {
 
         String REQUEST = createRequestAsString();
 
@@ -64,22 +141,105 @@ public class StartInformationControllerTest {
                 .andExpect(status()
                         .isOk())
                 .andReturn();
+
         String body = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-        Optional<StartInformation> byId = startInformationRepository.findById((short) 1);
         ObjectMapper objectMapper = new ObjectMapper();
         StartInformationResponseDTO startInformationResponseDTO = objectMapper.readValue(body, StartInformationResponseDTO.class);
-        StartInformation startInformation = startInformationResponseDTO.getStartInformationList().get(0);
+        StartInformation startInformationResponse = startInformationResponseDTO.getStartInformationList().get(0);
+
+        StartInformation startInformationRepositoryById = startInformationRepository.findById((short) 1)
+                .orElseThrow(() -> new NoSuchElementException("No value present"));
 
 
-        Assertions.assertEquals(byId.orElseThrow(() -> new NoSuchElementException("No value present")).getAvgDailyActivePower(),  startInformation.getAvgDailyActivePower());
-        Assertions.assertEquals(byId.orElseThrow(() -> new NoSuchElementException("No value present")).getAvgDailyReactivePower(),  startInformation.getAvgDailyReactivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getName(), startInformationResponse.getName());
+        Assertions.assertEquals(startInformationRepositoryById.getActivePower(), startInformationResponse.getActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAmount(), startInformationResponse.getAmount());
+        Assertions.assertEquals(startInformationRepositoryById.getKi(), startInformationResponse.getKi());
+        Assertions.assertEquals(startInformationRepositoryById.getCosf(), startInformationResponse.getCosf());
+        Assertions.assertEquals(startInformationRepositoryById.getTgf(), startInformationResponse.getTgf());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyActivePower(), startInformationResponse.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyReactivePower(), startInformationResponse.getAvgDailyReactivePower());
     }
 
+    @Test
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql", "/sql/addStartInformation.sql"})
+    public void updateStartInformation() throws Exception {
 
+        String REQUEST = updateRequestAsString();
+
+        MvcResult mvcResult = mockMvc.perform(put("/update")
+                        .content(REQUEST)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        StartInformationResponseDTO startInformationResponseDTO = objectMapper.readValue(body, StartInformationResponseDTO.class);
+        StartInformation startInformationResponse = startInformationResponseDTO.getStartInformationList().get(0);
+
+        StartInformation startInformationRepositoryById = startInformationRepository.findById((short) 1)
+                .orElseThrow(() -> new NoSuchElementException("No value present"));
+
+
+        Assertions.assertEquals(startInformationRepositoryById.getName(), startInformationResponse.getName());
+        Assertions.assertEquals(startInformationRepositoryById.getActivePower(), startInformationResponse.getActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAmount(), startInformationResponse.getAmount());
+        Assertions.assertEquals(startInformationRepositoryById.getKi(), startInformationResponse.getKi());
+        Assertions.assertEquals(startInformationRepositoryById.getCosf(), startInformationResponse.getCosf());
+        Assertions.assertEquals(startInformationRepositoryById.getTgf(), startInformationResponse.getTgf());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyActivePower(), startInformationResponse.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyReactivePower(), startInformationResponse.getAvgDailyReactivePower());
+    }
 
     @Test
-    public void protectiveEquipmentServiceClient_checkAvailability() {
-        Mockito.when(protectiveEquipmentServiceClient.checkAvailability(ArgumentMatchers.anyShort())).thenReturn(false);
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql", "/sql/addStartInformation.sql"})
+    public void deleteStartInformationById() throws Exception {
+
+
+        MvcResult mvcResult = mockMvc.perform(delete("/delete/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        StartInformationResponseDTO startInformationResponseDTO = objectMapper.readValue(body, StartInformationResponseDTO.class);
+        StartInformation startInformationResponse = startInformationResponseDTO.getStartInformationList().get(0);
+
+        StartInformation startInformationRepositoryById = startInformationRepository.findById((short) 1)
+                .orElseThrow(() -> new NoSuchElementException("No value present"));
+
+
+        Assertions.assertEquals(startInformationRepositoryById.getName(), startInformationResponse.getName());
+        Assertions.assertEquals(startInformationRepositoryById.getActivePower(), startInformationResponse.getActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAmount(), startInformationResponse.getAmount());
+        Assertions.assertEquals(startInformationRepositoryById.getKi(), startInformationResponse.getKi());
+        Assertions.assertEquals(startInformationRepositoryById.getCosf(), startInformationResponse.getCosf());
+        Assertions.assertEquals(startInformationRepositoryById.getTgf(), startInformationResponse.getTgf());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyActivePower(), startInformationResponse.getAvgDailyActivePower());
+        Assertions.assertEquals(startInformationRepositoryById.getAvgDailyReactivePower(), startInformationResponse.getAvgDailyReactivePower());
+    }
+
+    @Test
+    @Sql(scripts = {"/sql/clearStartInformationDB.sql", "/sql/addStartInformation.sql"})
+    public void checkAvailability() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(get("/check/2"))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+
+        boolean fromRepository = startInformationRepository.existsById((short) 2);
+        String body = mvcResult.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        boolean fromResponse = objectMapper.readValue(body, Boolean.class);
+
+        Assertions.assertEquals(fromRepository, fromResponse);
     }
 
 
@@ -92,7 +252,14 @@ public class StartInformationControllerTest {
 
     }
 
+    private String updateRequestAsString() throws JsonProcessingException {
+        StartInformationRequestDTO startInformationRequestDTO = new StartInformationRequestDTO((short) 1,
+                "Станок сверлильный", 12.5F, (short) 3, 0.26F, 0.5F, 1.73F);
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        return objectMapper.writeValueAsString(startInformationRequestDTO);
+
+    }
 
 
 }
